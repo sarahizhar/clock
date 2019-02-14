f4bd50cb7da919115c0c1c82a668687af0c5d094
sarahizhar <sarah.izhar@gmail.com>
2019-02-14 11:23:23 +0200

    background image changes with time

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 
+if (hr<19)
+{
+document.body.style.backgroundImage="url('day-img.jpg')";
+}
+else {
+  document.body.style.backgroundImage="url('night-img.jpg')";
+}
+
 let hrPosition = (hr*360/12)+(min*(360/60)/12);
 let minPosition = (min*360/60)+(sec*(360/60)/60);
 let secPosition = sec*360/60;
+
+
 
 function runTheClock() {
 
